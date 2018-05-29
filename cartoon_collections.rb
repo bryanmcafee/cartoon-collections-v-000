c8cfdd205309332175662a7352119ff012d1ba46
Bryan McAfee <brymcafee@gmail.com>
2018-05-29 14:43:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,10 @@ def long_planeteer_calls(array)
 value == nil ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types.each do |cheese|
+      return cheese if array.includes?(cheese) == true
 end
+end
+
