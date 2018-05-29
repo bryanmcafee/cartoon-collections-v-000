1ad39368453a01c486ffc45f1bfcb57486e97e0a
Bryan McAfee <brymcafee@gmail.com>
2018-05-29 14:44:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(array)
     cheese_types = ["cheddar", "gouda", "camembert"]
     cheese_types.each do |cheese|
-      return cheese if array.includes?(cheese) == true
+      return cheese if array.include?(cheese) == true
 end
 end
