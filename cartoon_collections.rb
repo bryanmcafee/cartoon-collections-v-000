521950619a7669cb030d6d7378f9e00068b91f11
Bryan McAfee <brymcafee@gmail.com>
2018-05-29 14:39:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  value = array.find{|i| i.length > 4}
+  value = array.find{|i| i.length < 4}
 value = nil ? false : true
 end
 
